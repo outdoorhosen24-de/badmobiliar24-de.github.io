65476d9508e68ca543a7a95b5f80aa86af7a5750
outdoorhosen24-de <outdoorhosen24-de@users.noreply.github.com>
2022-09-27 16:05:57 +0000

    deploy: db97e291fd93c40ed28b6ebea1a3bae1a0f5b9ee

---
+++ assets/static/1664294718/marken/posseik-mit-villeroy-boch/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fassets\u002Fstatic\u002F1664294718",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmarken\u002Fposseik-mit-villeroy-boch\u002F",config:{_app:{basePath:"\u002F",assetsPath:"\u002Fassets\u002F",cdnURL:a}}}}(null));
